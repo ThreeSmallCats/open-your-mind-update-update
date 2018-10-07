c1e8ada9cb74d1904cdda24d7bea0242e74d6079
luo <437986376@qq.com>
2018-10-07 15:49:12 +0800

    修改接口提示

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ window.onload = function () {
     // 网站跳转
     let docwidth = window.screen.availWidth
     if (docwidth <= 640) {
-        window.location.href = 'https://threesmallcats.cn/phone'
+        window.location.href = 'https://tscats.cn/openphone'
     }
      window.addEventListener('resize', function () {
         //  屏幕可用工作区宽度
@@ -14,7 +14,7 @@ window.onload = function () {
         //  console.log(docW)
          if (docW <= 640) {
 
-             window.location.href = 'https://threesmallcats.cn/phone'
+             window.location.href = 'https://tscats.cn/openphone'
          }
      })
     
